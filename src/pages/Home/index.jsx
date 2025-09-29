0010187452a4959553673c96e08e1e2aa53de686
ChangedRuby <gugasboy7@gmail.com>
2025-09-29 14:55:51 -0300

    added open session by ID

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
 import styles from './style.module.scss'
-import { createSession, sendMsgToSession } from '../../services/sessionService.js'
+import { createSession, sendMsgToSession, retrieveSession } from '../../services/sessionService.js'
 
 function Home() {
 
   const navigate = useNavigate();
+  const inputMessage = useRef();
 
   async function handleCreateSession() {
     const post = {
@@ -18,6 +19,20 @@ function Home() {
       navigate('/give-feedback', { state: { responseData: response } });
     } catch (err) {
       console.log(err);
+    }
+  }
+
+  async function handleOpenSessionByID() {
+    try {
+      const sessionID = inputMessage.current.value;
+      const response = await retrieveSession(sessionID);
+      console.log(response);
+
+      if(response){
+        navigate('/give-feedback', { state: { responseData: { id: sessionID } } });
+      }
+    } catch (err) {
+      console.log("Error when checking session", err);
     }
   }
   
@@ -35,6 +50,10 @@ function Home() {
       <div className={styles.form}>
         <h1>Welcome to HungryFood's Feedback Page</h1>
         <button onClick={handleCreateSession}>Open chat ticket</button>
+        <hr></hr>
+        <input name='mensagem' type='text' placeholder='Ticket ID' ref={inputMessage}></input>
+        <button onClick={handleOpenSessionByID}>Go to chat by ID</button>
+        <hr></hr>
         <button onClick={handleFeedbackHistoryNavigation}>See feedbacks</button>
       </div>
       <footer className={styles.footer}>&copy; 2025 Gustavo Macêdo. All rights reserved.</footer>
